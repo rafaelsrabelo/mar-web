3789d947a59933dd70206852c20857ea812c9122
Rafael Santana Rabelo <rafaelrabelo1610@gmail.com>
2024-01-17 12:05:10 -0300

    fix(api.ts): update base URL to point to the correct API endpoint
    The base URL in the `api.ts` file was updated to fix the incorrect API endpoint. The new base URL is now `https://bfdf-2804-14c-de87-83e7-c958-1398-2194-3236.ngrok-free.app/api/v1`.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://f977-2804-14c-de87-83e7-c958-1398-2194-3236.ngrok-free.app/api/v1',
+  baseURL: 'https://bfdf-2804-14c-de87-83e7-c958-1398-2194-3236.ngrok-free.app/api/v1',
 });
